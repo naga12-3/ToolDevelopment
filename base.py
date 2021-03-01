280f95322d88508acba79d6b832980dd116f0996
NAGA LOKESH <54976366+naga12-3@users.noreply.github.com>
2021-03-01 11:20:18 +0530

    Updated based on issue1

--- base.py
+++ base.py
@@ -3,24 +3,36 @@ import os
 import csv
 import sys
 from pprint import pprint
+
 # name of the repository
-repo_name="ethereum/go-ethereum"
+repo_names=['ChainSafe/web3.js', 'ethereum/solidity', 'openethereum/parity-ethereum', 'ethereum/EIPs', 'ethereum/wiki',
+            'ethereum/ethereum-org-website', 'ethereum/web3.py', 'ethereum/evmone', 'ethereum/remix-project', 'ethereum-mining/ethminer',
+            'ethereum/aleth', 'web3j/web3j', 'status-im/status-react', 'trufflesuite/ganache-cli', 'trufflesuite/ganache', 'ethers-io/ethers.js',
+            'prysmaticlabs/prysm', 'ethereum/eth2.0-specs', 'trustwallet/trust-wallet-ios', 'TokenMarketNet/smart-contracts',
+            'Nethereum/Nethereum', 'ethereum/py-evm', 'sammy007/open-ethereum-pool', 'ethereum/yellowpaper', 'poanetwork/blockscout',
+            'graphprotocol/graph-node', 'cubedro/eth-netstats', 'ethereum-ts/TypeChain', 'aragon/client', 'ethereumjs/ethereumjs-wallet',
+            'district0x/ethlance']
 
-token = os.getenv('GITHUB_TOKEN', '787f85f88e4bc128250495eb201652d3b616af7f')
+token = os.getenv('GITHUB_TOKEN', 'Access Token') 
 g = Github(token)
-repo = g.get_repo(repo_name)
-issues = repo.get_issues(state='all')
+for repo_name in repo_names:
+    repo = g.get_repo(repo_name)
+    issues = repo.get_issues(state='all')
 
-fields = ['issue', 'number', 'state','link']
-# copying all the issues to a csv file
-with open("first_data_ethereum_go-ethereum_1.csv", "w", encoding='utf-8') as file:
-    csv_file = csv.writer(file)
-    cols = fields
-    csv_file.writerow(cols)
-    for issue in issues:
-        row=[issue.title,issue.number,issue.state,'https://github.com/ethereum/go-ethereum/issues/'+str(issue.number)]
-        csv_file.writerow(row)
-      
-#for issue in issues[:10]:
-    #row = [issue.title, issue.number, issue.state, 'https://github.com/ethereum/go-ethereum/issues/' + str(issue.number)]
-    #print(row)
+    fields = ['issue', 'number', 'state', 'link']
+
+    # copying all the issues to a csv file
+
+    filename=(repo_name+'.csv').replace('/','_')
+    with open(filename, "w", encoding='utf-8') as file:
+        csv_file = csv.writer(file)
+        cols = fields
+        csv_file.writerow(cols)
+        for issue in issues:
+            row = [issue.title, issue.number, issue.state,
+                   'https://github.com/'+repo_name+'/issues/' + str(issue.number)]
+            csv_file.writerow(row)
+
+# for issue in issues[:10]:
+# row = [issue.title, issue.number, issue.state, 'https://github.com/ethereum/go-ethereum/issues/' + str(issue.number)]
+# print(row)
